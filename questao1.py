386fb6e2f1e18a21117f6bf8bdd75e499bb9bedf
Pedro Senna <pedro.vinnic@gmail.com>
2024-03-07 21:36:16 -0300

    questão 1

---
+++ questao1.py
@@ -0,0 +1,13 @@
+indice = 13
+soma = 0
+k = 0
+
+'''Aqui eu uso um laço de repetição que enquanto K for menor do que 
+INDICE o programa vai executar novamente linha por linha do meu código até
+o valor de K ser igual a 13 e a execução termina com o valor de 91'''
+
+while k < indice:
+    k = k + 1
+    soma = soma + k
+    print(soma)
+    
